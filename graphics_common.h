d413371e62598c0dc3b9b35f63b9ec297c9b2003
cosmicchasm <aidanvstanford@gmail.com>
2026-08-19 00:26:50 -0500

    Started graphics_common.h, hopefully should be good
    To integrate, include "graphics_common.h" in your application code.
    Currently only the SSD1306 is supported.
    
    Aidan S

---
+++ graphics_common.h
@@ -0,0 +1,39 @@
+/*
+ * Author: Aidan S
+ *
+ * Common header file for greater portability among
+ * other projects
+ */
+
+#ifndef __GRAPHICS__
+#define __GRAPHICS__
+
+#include "graphics_core.h"
+#include "oled_core.h"
+
+/* Device-specific macros */
+#define BYTE_FROM_XY(X,Y) XY2OL_BYTE(X,Y)
+#define BIT_FROM_XY(X,Y)  (1<<(XY2OL_BIT(X,Y)))
+
+#define SCREEN_LIMIT_X OLED_SIZE_W
+#define SCREEN_LIMIT_Y OLED_SIZE_L
+
+/* "Portable" graphic functions (may change with different core headers) */
+__inline void set_fb_pixel(int x, int y, uint8_t const *fb) {
+  if ((x < 0) || (x >= SCREEN_LIMIT_X) || 
+      (y < 0) || (y >= SCREEN_LIMIT_Y)) {
+    return;
+  } else {
+    fb[BYTE_FROM_XY(x,y)] |= BIT_FROM_XY(x,y);
+  }
+}
+
+__inline void clr_fb_pixel(int x, int y, uint8_t const *fb) {
+  if ((x < 0) || (x >= SCREEN_LIMIT_X) || 
+      (y < 0) || (y >= SCREEN_LIMIT_Y)) {
+    return;
+  } else {
+    fb[BYTE_FROM_XY(x,y)] &= ~BIT_FROM_XY(x,y);
+  }
+}
+#endif
